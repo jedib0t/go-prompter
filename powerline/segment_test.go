73e0885d646e8d86e94361067281dccee2b3e541
Naveen Mahalingam <jedib0t@outlook.com>
2023-09-26 11:47:48 -0700

    fix some more code smells

--- powerline/segment_test.go
+++ powerline/segment_test.go
@@ -13,12 +13,13 @@ func TestSegment_Render(t *testing.T) {
 
 	s.SetContent(testIP)
 	assert.True(t, s.hasChanges)
-	assert.Equal(t, "\x1b[38;5;7;48;5;91m "+testIP+" \x1b[0m", s.Render())
+	s.contentColor.Sprint(" " + testIP + " ")
+	assert.Equal(t, s.contentColor.Sprint(" "+testIP+" "), s.Render())
 	assert.False(t, s.hasChanges)
 
 	s.SetIcon("🌐")
 	assert.True(t, s.hasChanges)
-	assert.Equal(t, "\x1b[38;5;7;48;5;91m 🌐 "+testIP+" \x1b[0m", s.Render())
+	assert.Equal(t, s.contentColor.Sprint(" "+s.icon+" "+testIP+" "), s.Render())
 	assert.False(t, s.hasChanges)
 
 	s.SetColor(prompt.Color{
@@ -26,6 +27,6 @@ func TestSegment_Render(t *testing.T) {
 		Background: termenv.ANSI256Color(111),
 	})
 	assert.True(t, s.hasChanges)
-	assert.Equal(t, "\x1b[38;5;0;48;5;111m 🌐 "+testIP+" \x1b[0m", s.Render())
+	assert.Equal(t, s.color.Sprint(" "+s.icon+" "+testIP+" "), s.Render())
 	assert.False(t, s.hasChanges)
 }
